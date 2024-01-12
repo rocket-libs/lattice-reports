a9dca15f0475d7cf4ec29e249fa4bcc8bdd5c584
Nyingi <nyingi.maina@gmail.com>
2024-01-13 01:45:05 +0300

    0.0.19

--- lib/lattice_reports_configuration.dart
+++ lib/lattice_reports_configuration.dart
@@ -7,7 +7,7 @@ import 'package:lattice_reports/Data/order_data_point_model.dart';
 import 'package:lattice_reports/Dataflow/environment_information.dart';
 import 'package:lattice_reports/Locations/Data/location_model.dart';
 import 'package:lattice_reports/Logging/logger_wrapper.dart';
-import 'package:lattice_reports/QuickOverview/UI/quick_overview.dart';
+import 'package:lattice_reports/QuickOverview/Blocstar/quick_overview_logic.dart';
 import 'package:lattice_reports/SalesList/Blocstar/sales_list_logic.dart';
 import 'package:lattice_reports/Strings/strings.dart';
 import 'package:lattice_reports/VendorLocations/Data/vendor_location_model.dart';
@@ -43,7 +43,7 @@ class LatticeReportsConfiguration {
 
     logicRegistry
       ..registerImplicit(() => SalesListLogic())
-      ..registerImplicit(() => const QuickOverview());
+      ..registerImplicit(() => QuickOverviewLogic());
 
     await AuthenticationMessenger()
         .configureAsync(authenticationInformation: authenticationInfo);
