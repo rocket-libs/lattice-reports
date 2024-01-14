b8c4a9c7425500ea8f5f65d619f0cc2c7a3cd57e
Nyingi <nyingi.maina@gmail.com>
2024-01-14 10:57:24 +0300

    0.0.22

--- lib/Revenues/Data/revenues_reader.dart
+++ lib/Revenues/Data/revenues_reader.dart
@@ -2,6 +2,7 @@ import 'package:flutter_guid/flutter_guid.dart';
 import 'package:lattice_reports/Data/order_data_point_model.dart';
 import 'package:lattice_reports/Dataflow/endpoint_caller.dart';
 import 'package:lattice_reports/NonNullable/non_nullable_extensions.dart';
+import 'package:preflection/Serializer.dart';
 
 class RevenuesReader {
   String get apiBaseRoute => "v1/Revenues/";
@@ -14,9 +15,10 @@ class RevenuesReader {
     final vendorLocationIdsSet =
         storeIds.map((e) => e.value.valueOrDefault()).toSet();
     final apiCaller = EndpointCaller.lattice();
-    final result = await apiCaller.getAsync<List<OrderDataPointModel>>(
+    final response = await apiCaller.getAsync<List<dynamic>>(
         '${apiBaseRoute}get-revenues-by-arbitrary-dates?dateOne=${dateOne.toIso8601String()}&dateTwo=${dateTwo.toIso8601String()}&vendorLocationIds=${vendorLocationIdsSet.join(',')}&aggregateSingleDayData=$aggregateSingleDayData',
         offlineDisplayLabel: null);
-    return result;
+    final result = Serializer.deserializeMany<OrderDataPointModel>(response);
+    return result.toNonNullList();
   }
 }
