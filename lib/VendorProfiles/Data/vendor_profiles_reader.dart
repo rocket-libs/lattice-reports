89f75efb9a13583e7c44fe8f73d06c3090424e0d
Nyingi <nyingi.maina@gmail.com>
2024-01-14 18:13:25 +0300

    Before upgrade of packages at Sun 14 Jan 2024 06:13:25 PM EAT

--- lib/VendorProfiles/Data/vendor_profiles_reader.dart
+++ lib/VendorProfiles/Data/vendor_profiles_reader.dart
@@ -2,21 +2,12 @@ import 'package:lattice_reports/Authentication/Messaging/authentication_messenge
 import 'package:lattice_reports/Dataflow/endpoint_caller.dart';
 import 'package:lattice_reports/Logging/logger_wrapper.dart';
 import 'package:lattice_reports/VendorProfiles/Data/vendor_profile_model.dart';
-import 'package:preflection/Preflectable.dart';
 import 'package:preflection/Serializer.dart';
 
 class VendorProfilesReader {
   String get _apiBaseRoute => "v1/VendorProfiles/";
 
   Future<VendorProfileModel> getAsync({bool fetchFromRemote = false}) async {
-    final list =
-        await getAllAsync<VendorProfileModel>(fetchFromRemote: fetchFromRemote);
-    if (list.isEmpty) return VendorProfileModel();
-    return list[0];
-  }
-
-  Future<List<TResult>> getAllAsync<TResult extends Preflectable<TResult>>(
-      {bool fetchFromRemote = false}) async {
     final userProfileId =
         AuthenticationMessenger().authenticationInformation.userProfileId;
     final apiCaller = EndpointCaller.lattice();
@@ -24,7 +15,8 @@ class VendorProfilesReader {
         '${_apiBaseRoute}get-by-id?vendorId=$userProfileId&includeArchived=true',
         offlineDisplayLabel: null);
     LoggerWrapper().i(mapResult);
-    final vendorProfileModel = Serializer.deserializeSingle<TResult>(mapResult);
-    return [vendorProfileModel as TResult];
+    final vendorProfileModel =
+        Serializer.deserializeSingle<VendorProfileModel>(mapResult);
+    return vendorProfileModel ?? VendorProfileModel(displayLabel: "");
   }
 }
